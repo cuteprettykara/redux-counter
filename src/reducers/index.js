b06ada4e4a9ac38e1ac93528a1534daef78a0d10
cuteprettykara <cuteprettykara@gmail.com>
2019-01-26 11:53:23 +0900

    멀티카운터 생성

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,75 @@
-import color from './color';
-import number from './number';
-import { combineReducers } from 'redux';
+import * as types  from '../actions/ActionTypes';
 
-/*
-  서브 리듀서들을 하나로 합칩니다.
-  combineReducers를 실행하고 나면, store 형태를
-  파라미터로 전달한 객체 모양대로 만듭니다.
-  {
-    numberData: {
+// [최초 변화를 일으키기 전] 가지고 있어야 할 초기 상태를 정의합니다.
+const initialState = {
+  counters: [
+    {
+      color: 'black',
       number: 0
-    },
-    colorData: {
-      color: 'black'
     }
-  }
-*/
-const reducers = combineReducers({
-  colorData: color,
-  numberData: number
-});
+  ]
+};
 
-export default reducers;
+function counter(state=initialState, action) {
+  // 레퍼런스 생성
+  const { counters } = state;
+
+  switch (action.type) {
+    case types.CREATE:
+      return {
+        counters: [
+          ...counters,
+          {
+            color: action.color,
+            number: 0
+          }
+        ]
+      }
+
+    case types.REMOVE:
+      return {
+        counters: counters.slice(0, counters.length-1)
+      }
+
+    case types.INCREMENT:
+      return {
+        counters: counters.map((counter, index) => 
+          (action.index === index)
+            ? {
+              number: ++counter.number,
+              color: counter.color
+            } 
+            : counter
+        )
+      }
+
+    case types.DECREMENT:
+      return {
+        counters: counters.map((counter, index) => 
+          (action.index === index)
+            ? {
+              number: --counter.number,
+              color: counter.color
+            } 
+            : counter
+        )
+      }
+
+    case types.SET_COLOR:
+      return {
+        counters: counters.map((counter, index) => 
+          (action.index === index)
+            ? {
+              number: counter.number,
+              color: action.color
+            } 
+            : counter
+        )
+      }
+  
+    default:
+      return state;
+  }
+}
+
+export default counter;
